4571b2d8a82ca783a84b037c06b25f37f37b02dd
mhusendev <husenrgc.96@gmail.com>
2023-05-25 15:48:00 +0700

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin:'https://innovation052023.pages.dev/', credentials: true,  preflightContinue: true, }))
+app.use(cors({ origin:'https://innovation052023.pages.dev', credentials: true,  preflightContinue: true, }))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
