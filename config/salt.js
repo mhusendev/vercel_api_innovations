20dd145aa812c659c8f9e5fdfa930c167755ab6a
mhusendev <husenrgc.96@gmail.com>
2023-05-25 15:46:22 +0700

    update

--- config/salt.js
+++ config/salt.js
@@ -10,11 +10,13 @@ const compare = async (password,hash) => {
     // let isValid = false
     //  const result = await bcrypt.compare(password, hash);
     let result 
+    console.table(password,hash)
     if(password == hash) {
         result = true
     } else {
         result = false
     }
+    console.log(result)
     return result
 }
 
