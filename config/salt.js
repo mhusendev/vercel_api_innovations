2f2dfcf9342b2b2e1fd5ec6198b035975d463498
mhusendev <husenrgc.96@gmail.com>
2023-05-25 14:24:08 +0700

    test

--- config/salt.js
+++ config/salt.js
@@ -1,20 +1,14 @@
-// const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt")
 
 const encrypt =  async (password) => {
-    //  const hash = await bcrypt.hash(password, 10);
-    let hash = password
+     const hash = await bcrypt.hash(password, 10);
+
     return hash
 }
 
 const compare = async (password,hash) => {
-    // let isValid = false
-    //  const result = await bcrypt.compare(password, hash);
-    let result 
-    if(password == hash) {
-        result = true
-    } else {
-        result = false
-    }
+    let isValid = false
+     const result = await bcrypt.compare(password, hash);
     return result
 }
 
